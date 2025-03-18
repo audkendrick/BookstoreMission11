ebb1c68bec5e94c5c3250c92c35c0f487b642e2b
Audra Kendrick <kendrica@byu.edu>
2025-03-18 14:27:38 -0600

    Initial Commit

---
+++ backend/BookProject.API/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookProject.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookProject.API.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        [HttpGet("AllBooks")]
+        public IActionResult GetProjects(int pageSize = 5, int pageNum = 1)
+        {
+            var something = _bookContext.Books
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalNumProjects = _bookContext.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                TotalProjects = totalNumProjects
+            };
+
+            return Ok(someObject);
+        }
+
+        //[HttpGet("FunctionalProjects")]
+        //public IEnumerable<Project> GetFunctionalProjects()
+        //{
+        //    var something = _bookContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+        //    return something;
+        //}
+    }
+}
---
+++ frontend/src/ProjectList.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+import { Project } from "./types/Project";
+
+function ProjectList() {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchBowler = async () => {
+      const response = await fetch(
+        `https://localhost:5000/Book/AllBooks?pageSize=${pageSize}&pageNum=${pageNum}`
+      );
+      const data = await response.json();
+      setProjects(data.projects);
+      setTotalItems(data.totalNumProjects);
+      setTotalPages(Math.ceil(totalItems / pageSize));
+    };
+    fetchBowler();
+  }, [pageSize, pageNum, totalItems]);
+
+  return (
+    <>
+      <h1> Book Store </h1>
+      <br />
+      {projects.map((p) => (
+        <div id="projectCard" className="card" key={p.bookID}>
+          <h3 className="card-title">{p.title}</h3>
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li>
+                <strong>Author: </strong>
+                {p.author}
+              </li>
+              <li>
+                <strong>Publisher: </strong>
+                {p.publisher}
+              </li>
+              <li>
+                <strong>ISBN: </strong>
+                {p.isbn} Individuals Served
+              </li>
+              <li>
+                <strong>Classification: </strong>
+                {p.classification}
+              </li>
+              <li>
+                <strong>Category: </strong>
+                {p.category}
+              </li>
+              <li>
+                <strong>Page Count: </strong>
+                {p.pageCount}
+              </li>
+              <li>
+                <strong>Price: </strong>
+                {p.price}
+              </li>
+            </ul>
+          </div>
+        </div>
+      ))}
+
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button
+          key={i + 1}
+          onClick={() => setPageNum(i + 1)}
+          disabled={pageNum === i + 1}
+        >
+          {i + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+
+      <br />
+      <label>
+        Results per page:
+        <select
+          value={pageSize}
+          onChange={(p) => {
+            setPageSize(Number(p.target.value));
+            setPageNum(1);
+          }}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </label>
+    </>
+  );
+}
+
+export default ProjectList;
